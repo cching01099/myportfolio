03b772cd2aceb2f2d929710db86e0b0568a757a3
cching01099 <cching01099@gmail.com>
2024-10-09 13:36:47 +0800

    feat: update About page contents

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useMemo} from "react";
 import {useTranslation} from "react-i18next";
 
 type Data = {
@@ -14,6 +14,14 @@ const About:React.FunctionComponent = () => {
   const { t } = useTranslation();
 
   const data:Data[] = [
+      {
+          id: "0",
+          period: "2023/08-2024/08",
+          company:  t("about_work_company"),
+          position: t("about_work_position"),
+          desc:  t("about_work_desc"),
+          desc_2:  t("about_work_desc2"),
+      },
     {
       id: "1",
       period: "2022/01-2022/08",
@@ -24,7 +32,7 @@ const About:React.FunctionComponent = () => {
     },
     {
       id: "2",
-      period: "2021/08-2021/12",
+      period: "2021/08-2022/01",
       company:  t("about_work_2_company"),
       position: t("about_work_2_position"),
       desc:  t("about_work_2_desc"),
@@ -44,8 +52,8 @@ const About:React.FunctionComponent = () => {
       <div className="section_title">
         <h1>{t("about")}</h1>
       </div>
-      <div className="section_container">
-        <div className="shape"></div>
+      <div className="section_container" >
+        <div className="shape" style={{width:"8%"}}></div>
         <div className="info">
           <div className="summary">
             <h3>
@@ -60,51 +68,68 @@ const About:React.FunctionComponent = () => {
             <h3>
               <i className="fa-solid fa-screwdriver-wrench"></i>  {t("about_skills")}
             </h3>
-            <ul className="skill">
-              <li>
-                <span> {t("about_frontEnd")} </span> <br />
-                1.  {t("about_frontEnd_desc_1")} <br />
-                2. {t("about_frontEnd_desc_2")} <br />
-                3. {t("about_frontEnd_desc_3")}
-              </li>
-              <li>
-                <span>{t("about_dataAnalysis")}</span> <br/>
-                1. {t("about_dataAnalysis_desc_1")} <br/>
-                2. {t("about_dataAnalysis_desc_2")}  <br/>
-              </li>
-              <li>
-                <span>{t("about_languageAbility")} </span>
-                <br/>
-                1. {t("about_languageAbility_desc")}
-              </li>
-            </ul>
+              <ul className="skill">
+                  <li>
+                      <span> {t("about_react")} </span> <br/>
+                      1. {t("about_react_desc_1")} <br/>
+                      2. {t("about_react_desc_2")} <br/>
+                      3. {t("about_react_desc_3")}
+                  </li>
+                  <li>
+                      <span>{t("about_js")}</span> <br/>
+                      1. {t("about_js_desc_1")} <br/>
+                      2. {t("about_js_desc_2")} <br/>
+                      3. {t("about_js_desc_3")}
+                  </li>
+                  <li>
+                      <span>{t("about_css")}</span> <br/>
+                      1. {t("about_css_desc_1")} <br/>
+                      2. {t("about_css_desc_2")} <br/>
+                      3. {t("about_css_desc_3")} <br/>
+                      4. {t("about_css_desc_4")} <br/>
+                  </li>
+                  <li>
+                      <span>{t("about_others")}</span> <br/>
+                      1. {t("about_others_desc_1")} <br/>
+                      2. {t("about_others_desc_2")} <br/>
+                      3. {t("about_others_desc_3")} <br/>
+                      4. {t("about_others_desc_4")} <br/>
+                      5. {t("about_others_desc_5")} <br/>
+                  </li>
+              </ul>
           </div>
         </div>
-        <div className="experience">
-       <div className="learning">
+          <div className="experience" style={{padding:"0"}}>
+              <div className="learning" style={{padding: "0.6rem 1.2rem 1.2rem 1.2rem",borderBottom:`2px dotted rgb(21, 78, 140)`}}>
                   <h3>
-                        <i className="fa-solid fa-book"></i>
-                            <span> {t("about_learning")}</span>
-            </h3>
-                    <div className="details">
-                         <p className="description">{t("about_learning_desc")}</p>
-                         <span className="period">2022/08-2023/03</span>
-            </div>
-          </div>
-           <div className="works">
-             <h3>
-             <i className="fa-solid fa-briefcase"></i>
-             <span> {t("about_works")}</span>
-            </h3>
-            {data.map((d) => {
-              return (
+                      <i className="fa-solid fa-book"></i>
+                      <span> {t("about_learning")}</span>
+                  </h3>
+                  <div className="details">
+                      <p className="description">{t("about_learning_desc_1")}</p>
+                      <span className="period">2022/08-2023/03</span>
+
+                  </div>
+                  <div className="details">
+                      <p className="description">{t("about_learning_desc_2")}</p>
+                      <span className="period">2015/09-2019/06</span>
+
+                  </div>
+              </div>
+              <div className="works" style={{padding: "0.4rem 1.2rem"}}>
+                  <h3>
+                      <i className="fa-solid fa-briefcase"></i>
+                      <span> {t("about_works")}</span>
+                  </h3>
+                  {data.map((d) => {
+                      return (
                         <div className="work">
                   <div className="period">
                     <span>{d.period}</span>
                   </div>
                   <div className="content">
                    <p className="company">
-                      {d.company} <span className="position">{d.position}</span>
+                      {d.position} <span className="position">{d.company}</span>
                     </p>
                     <p className="description">
                       1. {d.desc} <br />
