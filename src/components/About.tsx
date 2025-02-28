e11506161f8a1e40dc69abffcaa39198bedfb575
cching01099 <cching01099@gmail.com>
2025-02-28 15:15:50 +0800

    feat: update new contents

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,59 +4,73 @@ import nd_logo from "../assets/logo/nd-logo.png"
 import carat_logo from "../assets/logo/carat-logo.png"
 import ogilvy_logo from "../assets/logo/ogilvy-logo.jpeg"
 import agein_logo from "../assets/logo/agein-logo.png"
+import astra_logo from "../assets/logo/astra-logo.jpeg"
+
 
 type Data = {
-    id:string
-    period:string
-    company:string
-    position:string
-    desc:string
-    desc_2:string
-    logo:string
+    id: string
+    period: string
+    company: string
+    position: string
+    desc: string[]
+    logo: string
 }
 
 const About:React.FunctionComponent = () => {
   const { t } = useTranslation();
 
-  const data:Data[] = [
-      {
-          id: "0",
-          period: "2023/08-2024/08",
-          company:  t("about_work_company"),
-          position: t("about_work_position"),
-          desc:  t("about_work_desc"),
-          desc_2:  t("about_work_desc2"),
-          logo:nd_logo
-      },
-    {
-      id: "1",
-      period: "2022/01-2022/08",
-      company:  t("about_work_1_company"),
-      position: t("about_work_1_position"),
-      desc:  t("about_work_1_desc"),
-      desc_2:  t("about_work_1_desc2"),
-        logo:carat_logo
-    },
-    {
-      id: "2",
-      period: "2021/08-2022/01",
-      company:  t("about_work_2_company"),
-      position: t("about_work_2_position"),
-      desc:  t("about_work_2_desc"),
-      desc_2:  t("about_work_2_desc2"),
-        logo:ogilvy_logo
-    },
-    {
-      id: "3",
-      period: "2020/05-2021/08",
-      company:  t("about_work_3_company"),
-      position: t("about_work_3_position"),
-      desc:  t("about_work_3_desc"),
-      desc_2:  t("about_work_3_desc2"),
-        logo:agein_logo
-    },
-  ];
-  return (
+    const data: Data[] = [
+        {
+            id: "0",
+            period: "2024/11-Now",
+            company: t("about_work_4_company"),
+            position: t("about_work_4_position"),
+            desc: [],
+            logo: astra_logo,
+        },
+        {
+            id: "1",
+            period: "2023/08-2024/08",
+            company: t("about_work_company"),
+            position: t("about_work_position"),
+            desc: [
+                t("about_work_desc"),
+                t("about_work_desc2"),
+                t("about_work_desc3"),
+                t("about_work_desc4"),
+                t("about_work_desc5"),
+                t("about_work_desc6"),
+                t("about_work_desc7"),
+            ],
+            logo: nd_logo,
+        },
+        {
+            id: "2",
+            period: "2022/01-2022/08",
+            company: t("about_work_1_company"),
+            position: t("about_work_1_position"),
+            desc: [t("about_work_1_desc"), t("about_work_1_desc2")],
+            logo: carat_logo,
+        },
+        {
+            id: "3",
+            period: "2021/08-2022/01",
+            company: t("about_work_2_company"),
+            position: t("about_work_2_position"),
+            desc: [t("about_work_2_desc"), t("about_work_2_desc2")],
+            logo: ogilvy_logo,
+        },
+        {
+            id: "4",
+            period: "2020/05-2021/08",
+            company: t("about_work_3_company"),
+            position: t("about_work_3_position"),
+            desc: [t("about_work_3_desc"), t("about_work_3_desc2")],
+            logo: agein_logo,
+        },
+    ]
+
+    return (
     <div id="about">
       <div className="section_title">
         <h1>{t("about")}</h1>
@@ -77,27 +91,28 @@ const About:React.FunctionComponent = () => {
             <h3>
               <i className="fa-solid fa-screwdriver-wrench"></i>  {t("about_skills")}
             </h3>
-              <ul className="skill" >
-                  <li style={{marginBottom:"1.5rem"}}>
+              <ul className="skill">
+                  <li style={{marginBottom: "1.5rem"}}>
                       <span> {t("about_react")} </span> <br/>
                       1. {t("about_react_desc_1")} <br/>
                       2. {t("about_react_desc_2")} <br/>
-                      3. {t("about_react_desc_3")}
+                      3. {t("about_react_desc_3")} <br/>
+                      4. {t("Next.js")}
                   </li>
-                  <li style={{marginBottom:"1.5rem"}}>
+                  <li style={{marginBottom: "1.5rem"}}>
                       <span>{t("about_js")}</span> <br/>
                       1. {t("about_js_desc_1")} <br/>
                       2. {t("about_js_desc_2")} <br/>
                       3. {t("about_js_desc_3")}
                   </li>
-                  <li style={{marginBottom:"1.5rem"}}>
+                  <li style={{marginBottom: "1.5rem"}}>
                       <span>{t("about_css")}</span> <br/>
                       1. {t("about_css_desc_1")} <br/>
                       2. {t("about_css_desc_2")} <br/>
                       3. {t("about_css_desc_3")} <br/>
                       4. {t("about_css_desc_4")} <br/>
                   </li>
-                  <li style={{marginBottom:"1.5rem"}}>
+                  <li style={{marginBottom: "1.5rem"}}>
                       <span>{t("about_others")}</span> <br/>
                       1. {t("about_others_desc_1")} <br/>
                       2. {t("about_others_desc_2")} <br/>
@@ -108,8 +123,9 @@ const About:React.FunctionComponent = () => {
               </ul>
           </div>
         </div>
-          <div className="experience" style={{padding:"0"}}>
-              <div className="learning" style={{padding: "0.6rem 1.2rem 1.2rem 1.2rem",borderBottom:`2px dotted rgb(21, 78, 140)`}}>
+          <div className="experience" style={{padding: "0"}}>
+              <div className="learning"
+                   style={{padding: "0.6rem 1.2rem 1.2rem 1.2rem", borderBottom: `2px dotted rgb(21, 78, 140)`}}>
                   <h3>
                       <i className="fa-solid fa-book"></i>
                       <span> {t("about_learning")}</span>
@@ -133,40 +149,34 @@ const About:React.FunctionComponent = () => {
                   {data.map((d) => {
                       return (
                         <div className="work">
-                  <div className="period">
+                  <div className="period" style={{width:'155px'}}>
                     <span>{d.period}</span>
                   </div>
                             <div style={{ display:"flex",flexDirection:"row-reverse"}}>
-                  <div className="content">
-                   <p className="company">
-                      {d.position} <span className="position">{d.company}</span>
-                    </p>
-                    <p className="description">
-                        {d.id === "0" ? (
-                            <>
-                                1. {d.desc} <br />
-                                2. {d.desc_2} <br />
-                                3. {t("about_work_desc3")} <br />
-                                4. {t("about_work_desc4")} <br />
-                                5. {t("about_work_desc5")} <br />
-                                6. {t("about_work_desc6")} <br />
-                                7. {t("about_work_desc7")} <br />
-                            </>
-                        ) : (
-                            <>
-                                1. {d.desc} <br />
-                                2. {d.desc_2}
-                            </>
-                        )}
-                    </p>
-                  </div>
-                            <div style={{padding:"0 0.4rem 0 0"}}><img src={d.logo} alt="company-logo" style={{width:"4rem"}}/></div>
+                                <div className="content">
+                                    <p className="company">
+                                        {d.position} <span className="position">{d.company}</span>
+                                    </p>
+                                    <p className="description">
+                                        {d.desc.length > 0 ? (
+                                            d.desc.map((item, index) => (
+                                                <span key={index}>
+                                                {index + 1}. {item} <br/>
+                                                </span>
+                                            ))
+                                        ) : (
+                                           <></>
+                                        )}
+                                    </p>
+                                </div>
+                                <div style={{padding: "0 0.4rem 0 0"}}><img src={d.logo} alt="company-logo"
+                                                                            style={{width: "4rem"}}/></div>
                             </div>
                         </div>
                       );
-            })}
+                  })}
+              </div>
           </div>
-        </div>
       </div>
     </div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,216 +1,127 @@
 import React from "react";
-import img1 from "../assets/portfolio.png";
 import img3 from "../assets/arcade-game.png";
 import img2 from "../assets/img-generator.png";
 import ac_img from "../assets/ac-platform.png";
 import er_img from "../assets/cems-er.png";
-import sdm_img from "../assets/cems-sdm.png";
-import node_img from "../assets/cems-node.png";
-import appWebview_img from "../assets/app-webview.png";
+import eatsy_img from "../assets/eatsy.png";
+
 
 
 
 import {useTranslation} from "react-i18next";
 
-interface Data  {
-  id:string
-  name:string
-  tag1:string
-  tag2:string
-  tag3:string
-  desc:string
-  img:string
-  demo:string
-  repo:string
+interface Data {
+    name: string
+    tags: string[]
+    desc: string
+    img: string
+    demo: string
+    repo: string
 }
 
 const Projects = () => {
   const { t } = useTranslation();
 
-  const projectDatas:Data[] = [
-    {
-    id: "1",
-          name:  t("projects_project_1_name"),
-          tag1: "MUI",
-          tag2: "Tailwind",
-          tag3: "工作作品",
-          desc: t("projects_project_1_desc"),
-          img: ac_img,
-          demo: "https://drive.google.com/file/d/1J3g9SqI_hpTEeCQ1w_MrdTCVMKN2QVNL/view",
-          repo: "",
-    },
-    {
-      id: "2",
-      name: t("projects_project_2_name"),
-        tag1: "MUI",
-        tag2: "Tailwind",
-        tag3: "工作作品",
-    desc: t("projects_project_2_desc"),
-    img: er_img,
-        demo: "",
-        repo: "",
-},
-{
-  id: "3",
-      name: t("projects_project_3_name"),
-    tag1: "MUI",
-    tag2: "Tailwind",
-    tag3: "工作作品",
-    desc: t("projects_project_3_desc"),
-    img: sdm_img,
-    demo: "",
-    repo: "",
-},
-      {
-          id: "4",
-          name:  t("projects_project_4_name"),
-          tag1: "MUI",
-          tag2: "Tailwind",
-          tag3: "工作作品",
-          desc: t("projects_project_4_desc"),
-          img: node_img,
-          demo: "",
-          repo: "",
-      },
-      {
-          id: "5",
-          name: t("projects_project_5_name"),
-          tag1: "MUI",
-          tag2: "Tailwind",
-          tag3: "工作作品",
-          desc: t("projects_project_5_desc"),
-          img: appWebview_img,
-          demo: "https://webview-dev.nextdrive.io/tw-webview/sscp-electricity-keeper?hash=6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b",
-          repo: "https://webview-dev.nextdrive.io/tw-webview/tpri-eesr-demand-response?hash=4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
-      },
-    {
-      id: "6",
-      name:  t("projects_project_6_name"),
-      tag1: "React",
-      tag2: "SCSS",
-      tag3: "個人作品",
-      desc: t("projects_project_6_desc"),
-      img: img1,
-      demo: "https://cching01099.github.io/myportfolio/",
-      repo: "https://github.com/cching01099/myportfolio",
-    },
-    {
-      id: "7",
-      name: t("projects_project_7_name"),
-      tag1: "React",
-      tag2: "SCSS",
-      tag3: "個人作品",
-      desc: t("projects_project_7_desc"),
-      img: img2,
-      demo: "https://cching01099.github.io/ImageGenerator-React/",
-      repo: "https://github.com/cching01099/ImageGenerator-React",
-    },
-    {
-      id: "8",
-      name: t("projects_project_8_name"),
-      tag1: "JS",
-      tag2: "SCSS",
-      tag3: "個人作品",
-      desc: t("projects_project_8_desc"),
-      img: img3,
-      demo: "https://cching01099.github.io/ArcadeGame/",
-      repo: "https://github.com/cching01099/ArcadeGame",
-    },
-  ];
+    const projectDatas:Data[] = [
+        {
+            name: t("projects_project_9_name"),
+            tags: ["Next.js", "Side Project"],
+            desc: t("projects_project_9_desc"),
+            img: eatsy_img,
+            demo: "https://partners.eatsy.tech/smart-booking-system",
+            repo: "",
+        },
+        {
+            name: t("projects_project_1_name"),
+            tags: ["React", "工作作品"],
+            desc: t("projects_project_1_desc"),
+            img: ac_img,
+            demo: "https://drive.google.com/file/d/1J3g9SqI_hpTEeCQ1w_MrdTCVMKN2QVNL/view",
+            repo: "",
+        },
+        {
+            name: t("projects_project_2_name"),
+            tags: ["React","工作作品"],
+            desc: t("projects_project_2_desc"),
+            img: er_img,
+            demo: "",
+            repo: "",
+        },
+        {
+            name: t("projects_project_8_name"),
+            tags: ["JS", "SCSS",'個人作品'],
+            desc: t("projects_project_8_desc"),
+            img: img3,
+            demo: "https://cching01099.github.io/ArcadeGame/",
+            repo: "https://github.com/cching01099/ArcadeGame",
+        },
+        {
+            name: t("projects_project_7_name"),
+            tags: ["React", "SCSS",'個人作品'],
+            desc: t("projects_project_7_desc"),
+            img: img2,
+            demo: "https://cching01099.github.io/ImageGenerator-React/",
+            repo: "https://github.com/cching01099/ImageGenerator-React",
+        },
+    ]
 
-  return (
-    <div id="projects">
-      <div className="section_title">
-        <h1>{t("projects")}</h1>
-      </div>
-      <div className="section_container">
-        <div className="project">
-          {projectDatas.map((d) => (
-              <div className="item" style={{cursor:"auto"}}>
-                <div className="info">
-                  <h1>
-                    0{d.id}.{" "}
-                    <div className="tag">
-                        <span className="tag1 ">{d.tag1}</span>
-                        <span className="tag2">{d.tag2}</span>
-                        <span className="tag3">{d.tag3}</span>
-                    </div>
-                  </h1>
-
-                  <h2>{d.name}</h2>
-
-                  <div className="others">
-                    <p>{d.desc}</p>
-                      {d.id === "5" ? (
-                          <div className="links">
-                              <a
-                                  href={d.demo}
-                                  target="_blank"
-                                  className="link"
-                                  rel="noopener noreferrer"
-                              >
-                                  <i className="fa-solid fa-globe"></i>demo1
-                              </a>
-                              <a
-                                  href={d.demo}
-                                  target="_blank"
-                                  className="link"
-                                  rel="noopener noreferrer"
-                              >
-                                  <i className="fa-solid fa-globe"></i>demo2
-                              </a>
-                          </div>
-                      ) : (
-                          (d.demo !== "" && d.repo !== "") ? (
-                              <div className="links">
-                                  <a
-                                      href={d.demo}
-                                      target="_blank"
-                                      className="link"
-                                      rel="noopener noreferrer"
-                              >
-                                  <i className="fa-solid fa-globe"></i>demo
-                              </a>
-                              <a
-                                  href={d.repo}
-                                  target="_blank"
-                                  className="link"
-                                  rel="noopener noreferrer"
-                              >
-                                  <i className="fa-brands fa-github"></i>repo
-                              </a>
-                          </div>
-                      ) : (d.demo !== "" && d.repo === "")  ? ( <div className="links">
-                          <a
-                              href={d.demo}
-                              target="_blank"
-                              className="link"
-                              rel="noopener noreferrer"
-                          >
-                              <i className="fa-solid fa-globe"></i>demo
-                          </a>
-                      </div>) : (<div style={{marginTop:"8px"}}><p style={{color:"grey",fontSize:"0.8rem"}}>{t("projects_reminder")}</p></div>))}
-                  </div>
+    return (
+        <div id="projects">
+            <div className="section_title">
+                <h1>{t("projects")}</h1>
+            </div>
+            <div className="section_container">
+                <div className="project">
+                    {projectDatas.map((d, index) => (
+                        <div key={index} className="item">
+                            <div className="info">
+                                <h1>
+                                    0{index + 1}.{" "}
+                                    <div className="tag">
+                                        {d.tags.map((tag, tagIndex) => (
+                                            <span key={tagIndex} className={`tag${tagIndex + 1}`}>
+                        {tag}
+                      </span>
+                                        ))}
+                                    </div>
+                                </h1>
+                                <h2>{d.name}</h2>
+                                <div className="others">
+                                    <p>{d.desc}</p>
+                                    {d.demo || d.repo ? (
+                                        <div className="links">
+                                            {d.demo && (
+                                                <a href={d.demo} target="_blank" className="link" rel="noopener noreferrer">
+                                                    <i className="fa-solid fa-globe"></i> demo
+                                                </a>
+                                            )}
+                                            {d.repo && (
+                                                <a href={d.repo} target="_blank" className="link" rel="noopener noreferrer">
+                                                    <i className="fa-brands fa-github"></i> repo
+                                                </a>
+                                            )}
+                                        </div>
+                                    ) : (
+                                        <div style={{ marginTop: "8px" }}>
+                                            <p style={{ color: "grey", fontSize: "0.8rem" }}>{t("projects_reminder")}</p>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="image">
+                                <img src={d.img} alt="" />
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                  <div className="image">
-                      <img src={d.img} alt=""/>
-                  </div>
-              </div>
-          ))}
+            </div>
+            <div className="section_more">
+                <a href="https://github.com/cching01099?tab=repositories" target="_blank" rel="noopener noreferrer">
+                    ...{t("projects_seeMore")}
+                </a>
+            </div>
         </div>
-      </div>
-
-        <div className="section_more">
-            <a
-                href="https://github.com/cching01099?tab=repositories"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                ...{t("projects_seeMore")}
-            </a>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Projects;
